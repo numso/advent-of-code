a260fd6dcb276e9d56e4d7fb3c591e0218e64172
Dallin Osmun <dosmun@gmail.com>
2015-12-20 21:43:44 -0700

    finished day twelve

---
+++ DayTwelve/Code.elm
@@ -0,0 +1,145 @@
+module DayTwelve.Code where
+
+import Json.Decode exposing (..)
+import DayTwelve.Input exposing (input, testInput)
+
+props : Decoder (List (String, Int))
+props =
+  oneOf
+    [ intList
+    , stringy
+    , (lazy (\_ -> sObject))
+    , (lazy (\_ -> sList))
+    , succeed []
+    ]
+
+sObject : Decoder (List (String, Int))
+sObject =
+  let keyVals = keyValuePairs props in
+    customDecoder keyVals stripKeys
+
+-- Uncomment this and use for part 2
+
+-- sObject : Decoder (List (String, Int))
+-- sObject =
+--   let keyVals = keyValuePairs props in
+--   let foo = customDecoder keyVals stripKeys in
+--     customDecoder foo checkForRed
+
+-- checkForRed a =
+--   let hasRed = List.any (\(str, _) -> str == "red") a in
+--     if hasRed then
+--       Ok []
+--     else
+--       Ok a
+
+sList : Decoder (List (String, Int))
+sList =
+  let listVals = list props in
+  let foo = customDecoder listVals flatten in
+    customDecoder foo stripReds
+
+stripReds a =
+  let withoutReds = List.filter (\(str, _) -> str /= "red") a in
+    Ok withoutReds
+
+stripKeys : List (String, List (String ,Int)) -> Result String (List (String ,Int))
+stripKeys =
+  Ok << List.foldl (\(_, nums) memo -> memo ++ nums) []
+
+flatten : List (List (String, Int)) -> Result String (List (String, Int))
+flatten =
+  Ok << List.foldl (++) []
+
+intList : Decoder (List (String, Int))
+intList =
+  customDecoder int (\a -> Ok [("", a)])
+
+stringy : Decoder (List (String, Int))
+stringy =
+  customDecoder string (\a -> Ok [(a, 0)])
+
+lazy : (() -> Decoder a) -> Decoder a
+lazy thunk =
+  customDecoder value
+      (\js -> decodeValue (thunk ()) js)
+
+
+run inp =
+  let res = decodeString sObject inp in
+    case res of
+      Ok nums ->
+        List.foldl (\(_, num) res -> res + num) 0 nums
+      Err err ->
+        let foo = Debug.log "Oh noes" err in
+          -1
+
+
+-- part1 input === 119433
+-- part2 input === 68466
+
+
+
+
+
+
+
+
+
+
+
+
+-- module DayTwelve.Code where
+
+-- import Json.Decode exposing (..)
+-- import DayTwelve.Input exposing (input, testInput)
+
+-- props : Decoder (List Int)
+-- props =
+--   oneOf
+--     [ intList
+--     , (lazy (\_ -> sObject))
+--     , (lazy (\_ -> sList))
+--     , succeed []
+--     ]
+
+-- sObject : Decoder (List Int)
+-- sObject =
+--   let keyVals = keyValuePairs props in
+--     customDecoder keyVals stripKeys
+
+-- sList : Decoder (List Int)
+-- sList =
+--   let listVals = list props in
+--     customDecoder listVals flatten
+
+-- stripKeys : List (String, List Int) -> Result String (List Int)
+-- stripKeys =
+--   Ok << List.foldl (\(_, nums) memo -> memo ++ nums) []
+
+-- flatten : List (List Int) -> Result String (List Int)
+-- flatten =
+--   Ok << List.foldl (++) []
+
+-- intList : Decoder (List Int)
+-- intList =
+--   customDecoder int (\a -> Ok [a])
+
+-- lazy : (() -> Decoder a) -> Decoder a
+-- lazy thunk =
+--   customDecoder value
+--       (\js -> decodeValue (thunk ()) js)
+
+
+-- part1 inp =
+--   let res = decodeString sObject inp in
+--     case res of
+--       Ok nums ->
+--         List.sum nums
+--       Err err ->
+--         let foo = Debug.log "Oh noes" err in
+--           -1
+
+
+-- -- part1 input === 119433
+-- -- part2 input === ???
---
+++ DayTwelve/Input.elm
@@ -0,0 +1,12 @@
+module DayTwelve.Input where
+
+import Native.InputTwelve
+
+input : String
+input = Native.InputTwelve.input
+
+testInput : String
+testInput = Native.InputTwelve.testInput
+
+testInput2 : String
+testInput2 = Native.InputTwelve.testInput2
